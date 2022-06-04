2c93f2bccbe1c39358f1572243e4d558d12aa656
ｔａｋａｔｔｏ <takatran29@gmail.com>
2022-06-05 01:19:43 +0700

    Add project files.

---
+++ ObfuscattorNET/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using ObfuscattorLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObfuscattorNET
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        public static Worker Worker { get; set; }
+    }
+}
